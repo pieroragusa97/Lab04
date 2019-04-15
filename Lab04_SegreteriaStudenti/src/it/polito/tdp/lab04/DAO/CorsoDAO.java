ae82d175fbbe16c6d643a3406e48d7b7e3fc7569
piero <piero@DESKTOP-9PAL0IS>
2019-04-15 14:09:14 +0200

    Ho riempito le classi Corso e Studente del model, e creato tutti i metodi nelle classi DAO. Rimane da creare il modello riprendendo le classiDAO e connetterlo al controller e al main

--- Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/DAO/CorsoDAO.java
+++ Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/DAO/CorsoDAO.java
@@ -36,29 +36,68 @@ public class CorsoDAO {
 
 				System.out.println(codins + " " + numeroCrediti + " " + nome + " " + periodoDidattico);
 
+				Corso c=new Corso(codins,numeroCrediti,nome,periodoDidattico);
+				corsi.add(c);
 				// Crea un nuovo JAVA Bean Corso
 				// Aggiungi il nuovo oggetto Corso alla lista corsi
 			}
 
-			return corsi;
+			conn.close();
 
 		} catch (SQLException e) {
 			// e.printStackTrace();
 			throw new RuntimeException("Errore Db", e);
 		}
+		return corsi;
 	}
 
 	/*
 	 * Dato un codice insegnamento, ottengo il corso
 	 */
-	public void getCorso(Corso corso) {
+	public Corso getCorso(String codice) {
+		String sql="SELECT * FROM Corso WHERE codins=codice";
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			ResultSet rs = st.executeQuery();
+			Corso c=new Corso(rs.getString("codins"),rs.getInt("crediti"),rs.getString("nome"),rs.getInt("pd"));
+		
+		    conn.close();
+		    return c;
+		    
+	       }catch(SQLException e) {
+		         throw new RuntimeException("Errore nel DB",e);
+	       }
 		// TODO
 	}
 
 	/*
 	 * Ottengo tutti gli studenti iscritti al Corso
 	 */
-	public void getStudentiIscrittiAlCorso(Corso corso) {
+	public List<Studente> getStudentiIscrittiAlCorso(Corso corso) {
+		String codice=corso.getCodins();
+		String sql="SELECT S.* FROM studente S, iscrizione I WHERE I.codins=codice AND S.matricola=I.matricola";
+		List<Studente> studenti=new LinkedList<Studente>();
+		
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()) {
+				Studente s=new Studente(rs.getInt("matricola"),rs.getString("nome"),rs.getString("cognome"),rs.getString("pd"));
+			    System.out.println(rs.getInt("matricola") +" "+ rs.getString("nome")+" " + rs.getString("cognome")+" " + rs.getString("pd"));
+			    studenti.add(s);
+			}
+			conn.close();
+			
+		}catch(SQLException e) {
+	         throw new RuntimeException("Errore nel DB",e);
+      }
+		return studenti;
+		
 		// TODO
 	}
 
@@ -66,6 +105,27 @@ public class CorsoDAO {
 	 * Data una matricola ed il codice insegnamento, iscrivi lo studente al corso.
 	 */
 	public boolean inscriviStudenteACorso(Studente studente, Corso corso) {
+		StudenteDAO sd=new StudenteDAO();
+		int mat=studente.getMatricola();
+		String codice=corso.getCodins();
+		String sql="INSERT INTO iscrizione SET matricola=mat,codins=codice";
+		List<Studente> studenticorso=this.getStudentiIscrittiAlCorso(corso);
+		
+		if(sd.getTuttiStudenti().contains(studente)&&!studenticorso.contains(studente)&&this.getTuttiICorsi().contains(corso)) {
+			
+		    try {
+			    Connection conn = ConnectDB.getConnection();
+			    PreparedStatement st=conn.prepareStatement(sql);
+			    ResultSet rs = st.executeQuery();
+			    conn.close();
+			    
+		    }catch(SQLException e) {
+		         throw new RuntimeException("Errore nel DB",e);
+		    }
+		    
+		    return true;
+		}
+		
 		// TODO
 		// ritorna true se l'iscrizione e' avvenuta con successo
 		return false;
--- Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/DAO/StudenteDAO.java
+++ Lab04_SegreteriaStudenti/src/it/polito/tdp/lab04/DAO/StudenteDAO.java
@@ -1,5 +1,52 @@
 package it.polito.tdp.lab04.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+import it.polito.tdp.lab04.model.Corso;
+import it.polito.tdp.lab04.model.Studente;
+
 public class StudenteDAO {
+	
+	public List<Studente> getTuttiStudenti() {
+
+		final String sql = "SELECT * FROM studente";
+
+		List<Studente> studenti = new LinkedList<Studente>();
+
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+
+				int matricola = rs.getInt("matricola");
+				String nome = rs.getString("nome");
+				String cognome = rs.getString("cognome");
+				String cds = rs.getString("CDS");
+
+				System.out.println(matricola + " " + nome + " " + cognome + " " + cds);
+
+				Studente s=new Studente(matricola,nome,cognome,cds);
+				studenti.add(s);
+				// Crea un nuovo JAVA Bean Studente
+				// Aggiungi il nuovo oggetto Studente alla lista studenti
+			}
+
+			conn.close();
+
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			throw new RuntimeException("Errore Db", e);
+		}
+		return studenti;
+	}
+
 
 }
